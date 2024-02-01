8ccaafb8c8e9f6c7cb12df3fbbf4c1bc278948aa
IndusAryan <serena01011975@gmail.com>
2024-02-01 17:14:24 +0530

    fix(twitter): try to fix shades of grey patch

--- src/main/kotlin/indus/org/patches/twitter/themes/ShadesOfGreyPatch.kt
+++ src/main/kotlin/indus/org/patches/twitter/themes/ShadesOfGreyPatch.kt
@@ -5,7 +5,6 @@ import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import java.io.FileWriter
 import java.nio.file.Files
 
 @Patch(
@@ -23,49 +22,49 @@ object ShadesOfGreyPatch  : ResourcePatch() {
         val valuesDirectory = resDirectory.resolve("values")
         if (!valuesDirectory.isDirectory) Files.createDirectories(valuesDirectory.toPath())
 
-        listOf(valuesDirectory).forEach { it ->
-            val stylesXml = it.resolve("styles.xml")
-
-            if (!stylesXml.exists()) {
-                FileWriter(stylesXml).use {
-                    it.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>")
-                }
-            }
-        }
-
         context.xmlEditor["res/values/styles.xml"].use { editor ->
             val document = editor.file
+            // Find the existing PaletteLightsOut style
+            val styles = document.getElementsByTagName("style")
+            for (i in 0 until styles.length) {
+                val styleNode = styles.item(i)
+                if (styleNode.attributes.getNamedItem("name")?.nodeValue == "PaletteLightsOut") {
 
-            val newStyle = document.createElement("style")
-            newStyle.setAttribute("name", "PaletteLightsOut")
-            newStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
+                    val styleItems = mapOf(
+                        "abstractColorCellBackground" to "#101010",
+                        "abstractColorCellBackgroundTranslucent" to "#101010",
+                        "abstractColorDeepGray" to "#ff7c838a",
+                        "abstractColorDivider" to "#ff2f3336",
+                        "abstractColorFadedGray" to "#101010",
+                        "abstractColorFaintGray" to "#101010",
+                        "abstractColorHighlightBackground" to "@color/black",
+                        "abstractColorLightGray" to "#ff2f3336",
+                        "abstractColorLink" to "@color/twitter_blue",
+                        "abstractColorMediumGray" to "#ffd9d9d9",
+                        "abstractColorText" to "@color/white",
+                        "abstractColorUnread" to "#ff051d2b",
+                        "abstractElevatedBackground" to "#ff1b2023",
+                        "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
+                    )
 
-            val styleItems = mapOf(
-                "abstractColorCellBackground" to "#101010",
-                "abstractColorCellBackgroundTranslucent" to "#101010",
-                "abstractColorDeepGray" to "#ff7c838a",
-                "abstractColorDivider" to "#ff2f3336",
-                "abstractColorFadedGray" to "#101010",
-                "abstractColorFaintGray" to "#101010",
-                "abstractColorHighlightBackground" to "@color/black",
-                "abstractColorLightGray" to "#ff2f3336",
-                "abstractColorLink" to "@color/twitter_blue",
-                "abstractColorMediumGray" to "#ffd9d9d9",
-                "abstractColorText" to "@color/white",
-                "abstractColorUnread" to "#ff051d2b",
-                "abstractElevatedBackground" to "#ff1b2023",
-                "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
-            )
+                    // Clear existing items in the style
+                    val childNodes = styleNode.childNodes
+                    for (j in childNodes.length - 1 downTo 0) {
+                        styleNode.removeChild(childNodes.item(j))
+                    }
 
-            styleItems.forEach { (k, v) ->
-                val styleElement = document.createElement("item")
+                    // Add the updated items to the existing style
+                    styleItems.forEach { (k, v) ->
+                        val styleElement = document.createElement("item")
+                        styleElement.setAttribute("name", k)
+                        styleElement.textContent = v
+                        styleNode.appendChild(styleElement)
+                    }
 
-                styleElement.setAttribute("name", k)
-                styleElement.textContent = v
-                newStyle.appendChild(styleElement)
+                    // Exit the loop after updating the style
+                    break
+                }
             }
-
-            document.getElementsByTagName("resources").item(0).appendChild(newStyle)
         }
     }
 }
